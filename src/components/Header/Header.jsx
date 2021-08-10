c91756b050bce5632752a3af6c7081e9bda20373
ChmyrRoma <chmyrroma.2002@gmail.com>
2021-08-10 17:43:34 +0300

    Add file SignIn

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import style from '../Header/Header.module.css'
 
 const Header = () => {
     return (
-        <div className={style.header}>Header</div>
+        <div className={style.header}>
+            <div>
+            </div>
+        </div>
     )
 }
 
--- src/components/Header/SignIn/SignIn.jsx
+++ src/components/Header/SignIn/SignIn.jsx
@@ -1,8 +1,25 @@
 import React from 'react';
+import { Grid, Button, Input } from '@material-ui/core';
+import style from './SignIn.module.css';
+import doctor from '../../../assets/images/doctor.png'
 
 const SignIn = () => {
     return (
-        <div>SignIn</div>
+        <div>
+            <Grid className={style.headerSingIn}>
+                <h1>Империя здоровья</h1>
+                <img src={doctor} />
+            </Grid>
+            <Grid container direction='column' alignItems='center' className={style.bodySignIn}>
+                <Grid container direction='column' alignItems='center' className={style.bodyInput}>
+                    <input placeholder='Логин' />
+                    <input placeholder='Пароль' />
+                </Grid>
+                <Grid>
+                    <button className={style.bodyButton}><h3>Войти</h3></button>
+                </Grid>
+            </Grid>
+        </div>
     )
 } 
 
