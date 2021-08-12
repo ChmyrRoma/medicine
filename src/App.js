44a3fae0558cb348ee9de8ec8d9a5118002dd8f4
Roma Gorshkov <evil.sierro@gmail.com>
2021-08-12 21:01:20 +0300

    create Header and NavBar

--- src/App.js
+++ src/App.js
@@ -7,39 +7,41 @@ import Alerts from './components/NavBar/Alerts/Alerts';
 import Clients from './components/NavBar/Clients/Clients';
 import Developments from './components/NavBar/Developments/Developments';
 import Messages from './components/NavBar/Messages/Messages';
+import NavBar from './components/NavBar/NavBar';
 import Receptions from './components/NavBar/Receptions/Receptions';
 import Staff from './components/NavBar/Staff/Staff';
 
 const App = () => {
   return (
-    
-    <div className = "app-wrapper">
+
+    <div className="app-wrapper">
       <Header />
-    <div className = "app-wrapper-content">
-      <Route path="/Receptions"
-        render={() => <Receptions />} 
-      />
-      <Route path="/Developments"
-          render={() => <Developments />} 
-      />
-      <Route path="/Alerts"
-        render={() => <Alerts />} 
-      />
-      <Route path="/Messages"
-        render={() => <Messages />} 
-      />
-      <Route path="/Clients"
-        render={() => <Clients />} 
-      />
-      <Route path="/Staff"
-        render={() => <Staff />} 
-      />
-      <Route path="/SignIn" 
-        render={() => <SignIn />}
-      />
-      <Route path="/SignUp" 
-        render={() => <SignUp />}
-      />
+      <NavBar />
+      <div className="app-wrapper-content">
+        <Route path="/receptions"
+          render={() => <Receptions />}
+        />
+        <Route path="/developments"
+          render={() => <Developments />}
+        />
+        <Route path="/alerts"
+          render={() => <Alerts />}
+        />
+        <Route path="/messages"
+          render={() => <Messages />}
+        />
+        <Route path="/clients"
+          render={() => <Clients />}
+        />
+        <Route path="/staff"
+          render={() => <Staff />}
+        />
+        <Route path="/signIn"
+          render={() => <SignIn />}
+        />
+        <Route path="/signUp"
+          render={() => <SignUp />}
+        />
       </div>
     </div>
   )
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,18 @@
+import { Grid } from '@material-ui/core';
 import React from 'react';
-import style from '../Header/Header.module.css'
+import style from '../Header/Header.module.css';
+import homeImg from '../assets/images/house.svg'
 
 const Header = () => {
     return (
-        <div className={style.header}>Header</div>
+        <Grid container xs={12}  className={style.header}>
+            <Grid xs={12} sm={6} className={style.headerBody}>
+                <div className={style.logo}>
+                    <img alt="" src={homeImg} />
+                    <div className={style.headerTitle}>Home</div>
+                </div>
+            </Grid>
+        </Grid>
     )
 }
 
