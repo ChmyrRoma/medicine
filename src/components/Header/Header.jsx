680019b0a2e66c582a13eb572787a0e3fcfa6fe9
ChmyrRoma <chmyrroma.2002@gmail.com>
2021-08-20 15:21:02 +0300

    update header, add adaptation and two NavLink

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,15 +2,23 @@ import React from 'react';
 import { Grid } from '@material-ui/core';
 import style from '../Header/Header.module.css';
 import homeImg from '../assets/images/house.svg';
+import { NavLink } from 'react-router-dom';
+
 
 const Header = () => {
     return (
-        <Grid className={style.header}>
-            <Grid className={style.headerBody}>
-                <div className={style.logo}>
-                    <img alt='' src={homeImg} />
-                    <div className={style.headerTitle}>Home</div>
-                </div>
+        <Grid container className={style.header}>
+            <Grid item xs={12} sm={12} xl={12} className={style.headerBody}>
+                <Grid container item xs={12} sm={12} xl={12} className={style.leftBlock}>
+                    <NavLink to='/' className={style.logo}>
+                        <img alt='' src={homeImg} />
+                        <div className={style.headerTitle}>Home</div>
+                    </NavLink>
+                </Grid>
+                <Grid item xs={6} sm={6} xl={12} className={style.bodyAuth}>
+                    <NavLink to='/signIn' className={style.bodyButton}>Sign In</NavLink>
+                    <NavLink to='/signUp' className={style.bodyButton}>SignUp</NavLink>
+                </Grid>
             </Grid>
         </Grid>
     )
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -14,45 +14,45 @@ const NavBar = () => {
     return (
         <div className={style.NavBar}>
             <Grid container  className={style.first}>
-            <Grid className={`${style.item} ${style.active} ${style.section}`}>
-                <NavLink activeClassName={style.activeLink} to = '/receptions'>
-                    <img alt='' src={receptionsImg} />
-                    <span className={style.sectionTitle}>Receptions</span>
-                </NavLink>
-            </Grid>
-            <Grid className={`${style.item} ${style.section}`}>
-                <NavLink activeClassName={style.activeLink} to = '/developments'>
-                    <img alt='' src={developmentsImg} />
-                    <span className={style.sectionTitle}>Developments</span>
-                </NavLink>
-            </Grid>
-            <Grid className={`${style.item} ${style.section}`}>
-                <NavLink activeClassName={style.activeLink} to = '/alerts'>
-                    <img alt='' src={alertsImg} />
-                    <span className={style.sectionTitle}>Alerts</span>
-                </NavLink>
-            </Grid>
+                <Grid className={`${style.item} ${style.active} ${style.section}`}>
+                    <NavLink activeClassName={style.activeLink} to = '/receptions'>
+                        <img alt='' src={receptionsImg} />
+                        <span className={style.sectionTitle}>Receptions</span>
+                    </NavLink>
+                </Grid>
+                <Grid className={`${style.item} ${style.section}`}>
+                    <NavLink activeClassName={style.activeLink} to = '/developments'>
+                        <img alt='' src={developmentsImg} />
+                        <span className={style.sectionTitle}>Developments</span>
+                    </NavLink>
+                </Grid>
+                <Grid className={`${style.item} ${style.section}`}>
+                    <NavLink activeClassName={style.activeLink} to = '/alerts'>
+                        <img alt='' src={alertsImg} />
+                        <span className={style.sectionTitle}>Alerts</span>
+                    </NavLink>
+                </Grid>
             </Grid>
             <Grid container  className={style.second}>
-            <Grid className={`${style.item} ${style.section}`}>
-                <NavLink activeClassName={style.activeLink} to = '/messages'>
-                    <img alt='' src={messagesImg} />
-                    <span className={style.sectionTitle}>Messages</span>
-                </NavLink>
+                <Grid className={`${style.item} ${style.section}`}>
+                    <NavLink activeClassName={style.activeLink} to = '/messages'>
+                        <img alt='' src={messagesImg} />
+                        <span className={style.sectionTitle}>Messages</span>
+                    </NavLink>
+                </Grid>
+                <Grid className={`${style.item} ${style.section}`}>
+                    <NavLink activeClassName={style.activeLink} to = '/clients'>
+                        <img alt='' src={clientsImg} />
+                        <span className={style.sectionTitle}>Clients</span>
+                    </NavLink>
+                </Grid>
+                <Grid className={`${style.item} ${style.section}`}>
+                    <NavLink activeClassName={style.activeLink} to = '/Staff'>
+                    <img alt='' src={staffImg}/> 
+                    <span className={style.sectionTitle}>Staff</span> 
+                    </NavLink>
+                </Grid>
             </Grid>
-            <Grid className={`${style.item} ${style.section}`}>
-                <NavLink activeClassName={style.activeLink} to = '/clients'>
-                    <img alt='' src={clientsImg} />
-                    <span className={style.sectionTitle}>Clients</span>
-                </NavLink>
-            </Grid>
-            <Grid className={`${style.item} ${style.section}`}>
-                <NavLink activeClassName={style.activeLink} to = '/Staff'>
-                   <img alt='' src={staffImg}/> 
-                   <span className={style.sectionTitle}>Staff</span> 
-                </NavLink>
-            </Grid>
-        </Grid>
         </div>
     )
 }
