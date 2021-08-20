f7ecec4623b4bb37c3eef08cdcd47d3429b61663
Roma Gorshkov <evil.sierro@gmail.com>
2021-08-20 15:53:28 +0300

    update input in SignUp

--- src/components/Header/SignUp/SignUp.jsx
+++ src/components/Header/SignUp/SignUp.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Grid } from '@material-ui/core';
 import nurse from '../../assets/images/nurse.svg';
 import style from '../SignUp/SignUp.module.css'
@@ -6,39 +6,34 @@ import { NavLink } from 'react-router-dom';
 
 const SignUp = () => {
 
-    const [checked, setChecked] = useState(false)
 
-    const addButton = () => {
-        setChecked(!checked)
-    }
-    
     return (
-        <div className={style.SignUp}>
-            <Grid className={style.headerSignUp}>
-                <h1>Empire of health</h1>
-                <img alt='' src={nurse} />
-            </Grid>
-            <Grid container direction='column' className={style.bodySignUp}>
-                <Grid container direction='column' className={style.bodyInput}>
-                    <input type='text' placeholder='Login' />
-                    <input type='email' placeholder='Email' />
-                    <input type='password' placeholder='Password' />
+        <div>
+            <form action=''>
+                <Grid className={style.headerSignUp}>
+                    <h1>Empire of health</h1>
+                    <img alt='' src={nurse} />
                 </Grid>
-                <Grid className={style.bodyConfirm}>
-                    <Grid className={style.checkAccept}>
-                        <input type='checkbox' onClick={addButton} />
-                        <h4 className={style.titleAccept}>Accept</h4> 
+                <Grid container direction='column' className={style.bodySignUp}>
+                    <Grid container direction='column' className={style.bodyInput}>
+                        <input type='text' placeholder='Login' maxLength='40' required />
+                        <input type='email' placeholder='Email' required />
+                        <input type='password' placeholder='Password' maxLength='40' minLength='8' required />
                     </Grid>
-                    <Grid >
-                    {checked ? <div className={style.checkedInput}><button><h3>SignUp</h3></button>
-                    <h5 className={style.titleSignUp}>Already a member?
+                    <Grid className={style.bodyConfirm}>
+                        <Grid className={style.checkAccept}>
+                            <input type='checkbox' required />
+                            <h4 className={style.titleAccept}>Accept</h4>
+                        </Grid>
+                        <button><h3>SignUp</h3></button>
+                        <h5 className={style.titleSignUp}>Already a member?
                         <NavLink to='/signIn'> Sign In </NavLink>
-                    </h5> </div> : undefined}
+                        </h5>
                     </Grid>
                 </Grid>
-            </Grid>
+            </form>
         </div>
     )
-} 
+}
 
 export default SignUp;
